ce2090d5dbfdd0628592f9fe775d4aa297313da3
Thet-Rain <92264416+Thet-Rain@users.noreply.github.com>
2022-06-20 15:20:50 -0400

    Init Comit

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+//Deefine paths for express config
+const publicDir = path.join(__dirname , '../public')
+const viewsPath = path.join(__dirname , '../templates/views')
+const partialsPath = path.join(__dirname , '../templates/partials')
+
+console.log(partialsPath)
+
+//Setup handlebars engine and views location
+app.set('view engine' , 'hbs');
+app.set('views' , viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Set up static directory to serve
+app.use(express.static(publicDir));
+
+//Use handle bar
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Thet Rain'
+    });
+})
+
+//about page with handle bar
+app.get('/about' , (req,res) => {
+    res.render('about', {
+        title: 'about me',
+        name: 'Thet Rain'
+    })
+})
+
+app.get('/help' , (req,res) => {
+    res.render('help', {
+        title: 'Help page',
+        message: 'This is help message',
+        name: 'Thet Rain'
+    })
+})
+
+
+
+app.get('/weather', (req,res) => {
+    if(!req.query.address){
+        return res.send({
+            error: "You must provide an address to search"
+        })
+    }
+
+    geocode(req.query.address ,  (error , {latitude, longitude, location} = {} )  => {
+        if (error){
+                res.send({ error })
+        }
+        else
+        {
+            forecast(latitude, longitude, (error, forecastData) => {
+                if (error){
+                    res.send({
+                        error: error
+                    })
+                }
+                else
+                {
+                    res.send({
+                        forecast: forecastData,
+                        location
+                    })
+                }
+           })
+        }        
+    })
+
+})
+
+
+
+//404 page for help
+app.get('/help/*', (req,res) => {
+    res.render('404page', {
+        title: '404Page',
+        error: 'Help article not found!',
+        name: 'Thet Rain'
+    })
+})
+//* get request for all 
+app.get('*', (req,res) => {
+    res.render('404page', {
+        title: '404 Page',
+        error: 'Page not found!',
+        name: 'Thet Rain'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (latitude , longitude, callback ) => {
+    const url = "http://api.weatherstack.com/current?access_key=14efa6ba370bdf6612d47be0c211a6b4&query=" + latitude + "," + longitude +"&units=f";
+    debugger
+    request({ url , json: true}, (error , {body}) =>{
+        if (error){
+            callback('Unable to connect to network', undefined)
+        }else if(body.error){
+            callback('Unable to find forecast! Try different address' , undefined)
+        }else{
+            const weather_descriptions =  body.current.weather_descriptions;
+            const temperature = body.current.temperature;
+            const feelslike = body.current.feelslike;
+            callback(undefined , "Weather - " + weather_descriptions + ". Temperature - " + temperature + ". FeelsLike - " + feelslike)
+        }
+    })
+
+}
+
+module.exports = forecast ;
